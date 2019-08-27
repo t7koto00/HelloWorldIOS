371308288c17ff5a5e7b2dab2975fe02e31f3787
t7koto00 <t7koto00@students.oamk.fi>
2019-08-27 18:32:16 +0300

    Eka commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Toni Kontio on 27/08/2019.
+//  Copyright © 2019 Toni Kontio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var HelloLabel: UILabel!
+    @IBAction func GreetButton(_ sender: Any) {
+        HelloLabel.text = "Hello " + TextField.text
+    }
+    @IBOutlet weak var TextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
